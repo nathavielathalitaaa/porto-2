b3cb79c0e85553e60870b52b3b92fc671ec59ec6
nathavielathalitaaa <nathaviela_thalita_ts7_24@student.smktelkom-sda.sch.id>
2026-07-07 12:37:51 +0700

    chore: configure github pages deployment scripts and base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/porto-2/',
   build: {
     chunkSizeWarningLimit: 4000,
   },
